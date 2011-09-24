4cd22cdbfcc306d8812a40a8d0e80c12608acb18
paws <me@onpaws.com>
2011-09-24 06:17:40 -0400

    first commit - following rails3 screencast tut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '29a69baa375486a64040dc18bebf8b745b3d81a8c2c7de2c4420e700333144de0838b831ebede7de3dda187c7d051f99f7de6f94448d959ec0a78c0c64631868'
